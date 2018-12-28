ecf43944062afb86ab2c40b0d0665b37753fad3b
caijiami <369634538@qq.com>
2018-12-28 17:10:26 +0800

    app_vue_cli

---
+++ vue_sever_00/db.sql
@@ -0,0 +1,15 @@
+SET NAMES UTF8;
+DROP DATABASE IF EXISTS disney;
+CREATE DATABASE disney CHARSET=UTF8;
+USE disney;
+
+/* 建表示例
+CREATE TABLE cake_users(
+    id INT,
+    uname VARCHAR(100),
+    phone VARCHAR(11),
+    upwd VARCHAR(100),
+    num INT
+);
+INSERT INTO cake_users VALUES
+*/
